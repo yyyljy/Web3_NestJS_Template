9216bfe38917321eaf90474421201dea81b722fe
jylee <yyyljy@gmail.com>
2024-08-20 14:47:06 +0900

    init 초기 세팅 추가
    solc, ethers, cors, .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `./.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 3000,
+        limit: 1,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 50,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 200,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    console.log('hi');
+    return 'Welcome to Web3 World!';
   }
 }
