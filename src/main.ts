69571933fb90bbd133b4dc948905eba0093d8825
jylee <yyyljy@gmail.com>
2024-08-30 15:44:34 +0900

    feat: add request size limit
    body size 100mb, url param size 1mb

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.enableCors();
+  app.use(bodyParser.json({ limit: '100mb' }));
+  app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
